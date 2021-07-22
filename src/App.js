669901f03d72070fbf346aad68006e50fc884215
Jeong Ga Hee <jghgahee@ajou.ac.kr>
2021-07-22 15:45:31 +0900

    modi

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+//import './App.css';
+//import PropTypes from 'prop-types';
+//import { withStyles } from '@material-ui/core/styles';
+//import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import AppBar from '@material-ui/core/AppBar';
+//import Toolbar from '@material-ui/core/Toolbar';
+//import MenuIcon from '@material-ui/icons/Menu';
+//import IconButton from '@material-ui/core/IconButton';
+//import ExitToApp from '@material-ui/icons/ExitToApp';
+//import Drawer from '@material-ui/core/Drawer';
+//import Forms from './Forms';
+//import HomeIcon from '@material-ui/icons/Home';
+import Typography from '@material-ui/core/Typography';
+
+
+export default class App extends React.Component {
+    render () {
+        return (
+            <AppBar postion="static">
+                <Typography align="center" variant="h3" color="inherit">Your Favorite Musics</Typography>
+            </AppBar>
+        );
+    }
+}
+
